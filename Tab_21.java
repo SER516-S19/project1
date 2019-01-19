d25b58c1886553845b0cb2169c108efdacba9708
Josh <jkdrumm1@gmail.com>
2019-01-19 15:07:49 -0700

    Added Tab_21 Class, the tab for Joshua Drumm

---
+++ Tab_21.java
@@ -0,0 +1,17 @@
+/*  
+    Class to create the Tab 21 and to contain all of the panels on Gold Team
+    Author: Joshua Drumm
+    E-Mail: jkdrumm@asu.edu
+    Date:   1/19/2019
+*/
+
+import javax.swing.JPanel;
+
+public class Tab_21 extends JPanel
+{
+    @Override
+    public String getName()
+    {
+        return "Josh Drumm";
+    }
+}
