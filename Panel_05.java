a6145691d108de6289de13ee6a5dbe718f2b3289
Jennytsd7 <jananitsd@gmail.com>
2019-01-19 16:30:26 -0700

    Update Panel_05.java

--- Panel_05.java
+++ Panel_05.java
@@ -9,7 +9,7 @@ public class Panel_05
    {
 	public Panel_05() 
 	   {
-         JFrame f= new JFrame("Panel Example");  //    For testing 
+         //JFrame f= new JFrame("Panel Example");  //    For testing 
          JPanel panel=new JPanel();  
 	     JLabel jlabel = new JLabel("JANANI_05",JLabel.CENTER);
 	     jlabel.setForeground(Color.white);
@@ -17,16 +17,16 @@ public class Panel_05
 	     panel.add(jlabel);
 	     panel.setBounds(10,10,230,20); 
  	     panel.setBorder(new LineBorder(Color.BLACK)); 
-		 panel.setBackground(Color.blue);  
-		 f.add(panel);  
-         f.setSize(500,400);    
+             panel.setBackground(Color.blue);  
+		 //f.add(panel);  
+         //f.setSize(500,400);    
          //f.setLayout(null);    		 
-         f.setVisible(true);    
+        // f.setVisible(true);    
 	  }
 
-	public static void main(String[] args)
-	   {
-		 new Panel_05();						//For Testing 
- 	   }
+	//public static void main(String[] args)
+	  // {
+//		 new Panel_05();						//For Testing 
+ //	   }
 
 }
