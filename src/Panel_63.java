4f359438eccc4ba446a7bb0aa844edcf6611e8c0
Dhruv Patel <drpatel8@asu.edu>
2019-01-20 13:46:41 -0700

    Added Panel_63 for Tab_48

---
+++ src/Panel_63.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class Panel_63 extends JPanel {
+	public Panel_63() {
+
+		createPanel();
+		
+	}
+
+	private createPanel(){
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 10, 250, 60);	//setting co-ordinates and size of the panel
+		panel.setBackground(Color.cyan);	//setting background color to cyan
+		panel.setBorder(BorderFactory.createLineBorder(Color.black, 1));	//setting border of the panel
+		
+		JLabel name = new JLabel("DHRUV PATEL"); 	//creating label with my name
+		name.setFont(new Font("Courier", Font.BOLD, 26));	//setting font to the label 
+		
+		panel.add(name);	//adding label into the panel
+		
+	}
+}
