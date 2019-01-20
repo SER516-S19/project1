a6404835c8bd63aa16a79eec7ef9d647612b30ff
arch92 <arch92@gmail.com>
2019-01-20 12:27:20 -0700

    Update Panel_49.java

--- Panel_49.java
+++ Panel_49.java
@@ -1,3 +1,4 @@
+
 /**
  * @author Archana Madhavan
  *
@@ -7,37 +8,36 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Panel_49 extends JPanel{
-Panel_49()
-{
-	//Creating Temporary Frame object
-	//JFrame Tmp_Frame= new JFrame();
-	
-	JPanel panel_49 = new JPanel();
-	
-	//Creating Label with necessary specifications
-	JLabel L = new JLabel("Archana Madhavan");
-	L.setFont(new Font("Verdana",Font.BOLD,24));
-	
-	//setting Panel specifications
-	panel_49.add(L);
-	panel_49.setBackground(Color.YELLOW);	
-	panel_49.setAlignmentX(L.CENTER_ALIGNMENT);
-	panel_49.setPreferredSize(new Dimension(300,200));
-	
-	
+public class Panel_49 extends JPanel {
+	Panel_49() {
+		/*
+		 * // Creating Temporary Frame object JFrame Tmp_Frame= new JFrame();
+		 */
+
+		JPanel panel_49 = new JPanel();
+
+		// Creating Label with necessary specifications
+		JLabel L = new JLabel("Archana Madhavan");
+		L.setFont(new Font("Verdana", Font.BOLD, 24));
+
+		// setting Panel specifications
+		panel_49.add(L);
+		panel_49.setBackground(Color.YELLOW);
+		//panel_49.setBounds(10, 10, 200, 200);
+
+		
 		/*
 		 * //Temporary Frame Tmp_Frame.add(panel_49); Tmp_Frame.setSize(400, 400);
 		 * Tmp_Frame.setVisible(true);
 		 */
-	
-		
-	
-}
+		 
+	}
 
+	
 	/*
 	 * public static void main(String[] args) { new Panel_49();
 	 * 
 	 * }
 	 */
+	 
 }
