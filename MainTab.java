a6bce540dd845fcb7fd649e59ef48e5a152f0c7e
Josh <jkdrumm1@gmail.com>
2019-01-19 19:17:59 -0700

    Tab_21 now shows Panels that have been added by users

--- MainTab.java
+++ MainTab.java
@@ -3,7 +3,10 @@ import java.awt.*;
 
 public class MainTab extends JPanel {
 
-    public MainTab(){}
+    public MainTab()
+    {
+        this.setLayout(new GridLayout(5,4));
+    }
 
     public  MainTab(String name) {
         this.setLayout(new GridLayout(5,4));
--- Tab_21.java
+++ Tab_21.java
@@ -5,11 +5,53 @@
     Date:   1/19/2019
 */
 
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class Tab_21 extends MainTab
 {
+    private static String[] panelNums = {"53", "82"};
+    
+    public Tab_21()
+    {
+        super();
+        addPanels();
+    }
+    
+    public Tab_21(String name)
+    {
+        super(name);
+        addPanels();
+    }
+    
     @Override
     public String getName()
     {
         return "Josh Drumm";
+    }
+
+    private void addPanel(String panelClassNum){
+        // takes the class name as parameter and if the class exists then adds 
+        // its object to the passed tabbedPane
+        try{
+            Class<?> clazz = Class.forName("Panel_" + panelClassNum);
+            Object newTab = clazz.getDeclaredConstructor().newInstance();
+            this.add((JPanel)newTab);
+        }
+        catch (Exception e){
+            JPanel newPanel = new JPanel();
+            newPanel.add(new JLabel("Panel Missing: " + panelClassNum));
+            this.add(newPanel);
+        }
+    }
+    
+    public void addPanels()
+    {
+        for(int i = 0; i < panelNums.length; i++)
+            addPanel(panelNums[i]);
+        for(int i = 0; i < 20 - panelNums.length; i++)
+            addPanel("Blank");
     }
 }
