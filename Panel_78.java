199531c508c10aa0931c70a1a7db02923c7adf3d
virajtalaty <virajtalaty@gmail.com>
2019-01-19 23:36:20 -0700

    Panel_78 after Update

--- Panel_78.java
+++ Panel_78.java
@@ -12,24 +12,15 @@ import javax.swing.border.LineBorder;
 public class Panel_78 extends JPanel
 {
 	
-	JPanel panel;
-	
 	Panel_78()  
     {  
-	    panel=new JPanel();  
 	    
 	    JLabel jlabel = new JLabel("Viraj Talaty");
 	    jlabel.setFont(new Font("Verdana",1,20));
 	    
-        panel.setBackground(Color.LIGHT_GRAY);
-        panel.setBorder(new LineBorder(Color.yellow));
-        panel.setVisible(true);
-        panel.add(jlabel);
-	    
+        setBackground(Color.LIGHT_GRAY);
+        setBorder(new LineBorder(Color.yellow));
+        setVisible(true);
+        add(jlabel);	    
     }
-
-	public JPanel getPanel() {
-		return panel;
-	}
-
 }
