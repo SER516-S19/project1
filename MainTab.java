8befd035a9fd301623ca152f5022753f7b947088
Josh <jkdrumm1@gmail.com>
2019-01-19 19:39:18 -0700

    Added Gridlayout in MainTab for both constructors

--- MainTab.java
+++ MainTab.java
@@ -3,10 +3,13 @@ import java.awt.*;
 
 public class MainTab extends JPanel {
 
-    public MainTab(){}
+    public MainTab()
+    {
+        this.setLayout(new GridLayout(5,4));
+    }
 
     public  MainTab(String name) {
-        this.setLayout(new GridLayout(5,4));
+        this();
         this.setName(name);
 
     }
