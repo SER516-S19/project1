9a598731e224ec764e446a25846d96b8be367aa2
Nikhila6 <saini.nikhila@gmail.com>
2019-01-20 15:46:17 -0700

    Removed setbounds

--- Panel_70.java
+++ Panel_70.java
@@ -18,14 +18,14 @@ public class Panel_70 extends JPanel {
         //JFrame frame = new JFrame("Example Panel"); //For Testing
         JPanel jPanel = new JPanel();
         JLabel jLabel = new JLabel("Nikhila Saini");
-   	    jLabel.setFont(new Font("Algerian", Font.BOLD, 24));
+   	jLabel.setFont(new Font("Algerian", Font.BOLD, 24));
         jPanel.add(jLabel);
 
         //frame.setSize(700, 600);
         //frame.add(jPanel);
 	    //frame.setVisible(true);
 
-        jPanel.setBounds(10, 10, 250, 60);
+        //jPanel.setBounds(10, 10, 250, 60);
         jPanel.setBackground(Color.CYAN);
         
     }
