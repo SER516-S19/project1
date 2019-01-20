1d02fd94d4c56685791376f7312ee1f53c2233cd
MayankBatra005 <37073720+MayankBatra005@users.noreply.github.com>
2019-01-20 11:58:14 -0700

    Update Panel_09.java

--- Panel_09.java
+++ Panel_09.java
@@ -13,11 +13,16 @@ public class Panel_09 extends JPanel
 {
 	Panel_09()  
     {  
- //   JFrame f= new JFrame("Panel Example");  //    For testing 
-    JPanel panel=new JPanel();  
+ //   JFrame f= new JFrame("Panel Example");  //    For testing
+        // Created a panel
+    JPanel panel=new JPanel();
+    // Created label
     JLabel jlabel = new JLabel("Mayank ID 09");
+    // Font defined for label
     jlabel.setFont(new Font("Verdana",1,20));
+    // added label to panel
     panel.add(jlabel);
+    // boder and background defined for panel
     panel.setBorder(new LineBorder(Color.BLACK)); 
     panel.setBounds(10,10,250,60);    
     panel.setBackground(Color.WHITE);  
