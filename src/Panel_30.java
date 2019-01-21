52a2d0e93ec83ee46ae1af4ec0d42adca82f11ae
Naga Sai Krishna Gurram <krishnaa.gsk@gmail.com>
2019-01-21 15:22:42 -0700

    Update Panel_30.java
    Removed commented code.

--- src/Panel_30.java
+++ src/Panel_30.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.border.Border;
+
 
 /* Objective of this class is to create a panel with label
 Label contains First name & Last name of the student */
@@ -17,7 +17,6 @@ Label contains First name & Last name of the student */
 
    public void createPanel() {
 
-     // JFrame frame = new JFrame();
      JPanel panel_30 = new JPanel();
      JLabel name = new JLabel("Krishna Gurram");
 
@@ -25,15 +24,6 @@ Label contains First name & Last name of the student */
      panel_30.add(name);
      name.setFont(new Font("Osaka", Font.PLAIN, 15));
 
-  /*  For testing
 
-     frame.add(panel_30);
-     frame.setSize(200,200);
-     frame.setVisible(true);
-   }
-
- public static void main(String[] args) {
-  Panel_30 panel = new Panel_30();
-} */
    }
  }
