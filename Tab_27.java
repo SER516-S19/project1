4db54c83ce7f0b56d46d5096fb2b9e15b71adf3e
Bharat Goel <bgoel1@asu.edu>
2019-01-21 14:47:22 -0700

    Changed panel 19 to 16 as mistaken by team member

--- Tab_27.java
+++ Tab_27.java
@@ -23,7 +23,7 @@ public class Tab_27 extends JPanel {
         this.setLayout(new GridLayout(4,5));
 
         myPanels = new String[]{"Panel_06", "Panel_18", "Panel_20", "Panel_59", "Panel_84", "Panel_05", "Panel_70",
-                "Panel_80", "Panel_17", "Panel_49", "Panel_55", "Panel_52", "Panel_65", "Panel_31", "Panel_19", "Panel_26",
+                "Panel_80", "Panel_17", "Panel_49", "Panel_55", "Panel_52", "Panel_65", "Panel_31", "Panel_16", "Panel_26",
                 "Panel_08", "Panel_39", "Panel_XX", "Panel_XX"};
 
         for(int i = 0; i<myPanels.length; i++){
