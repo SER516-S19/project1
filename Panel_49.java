ecd61018533ac6cae6c92ff2f5142ca479c60d33
arch92 <arch92@gmail.com>
2019-01-21 15:10:10 -0700

    Removed Commented Code

--- Panel_49.java
+++ Panel_49.java
@@ -10,36 +10,19 @@ import javax.swing.*;
 
 public class Panel_49 extends JPanel {
 	public Panel_49() {
-		
 		//Calling function to create panel
 		AddPanel_49();
-		
 	}
-	
-	
-	//adding panel		
+		
 	public void AddPanel_49() {
 		
-		//JPanel panel_49 = new JPanel();
-		/*
-		 * // Creating Temporary Frame object JFrame Tmp_Frame= new JFrame();
-		 * 
-		 * //Temporary Frame
-		 * Tmp_Frame.add(panel_49); 
-		 * Tmp_Frame.setSize(400, 400);
-		 * Tmp_Frame.setVisible(true);
-		 */
-		// Creating Label with necessary specifications
+		//Creating Label with necessary specifications
 		JLabel L = new JLabel("Archana Madhavan");
 		L.setFont(new Font("Verdana", Font.BOLD, 24));
 
-		// setting Panel specifications
+		//setting Panel specifications
 		this.add(L);
 		this.setBackground(Color.YELLOW);
-		//panel_49.setBounds(10, 10, 200, 200);
-		
 	}
-
-		 
 	 
 }
