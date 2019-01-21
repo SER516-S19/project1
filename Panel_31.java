3bb0f9b21eb38ac3c83e1f7edf9fc0e3825232c6
srivanreddy <sgutha3@asu.edu>
2019-01-21 15:06:56 -0700

    Removed unwanted code

--- Panel_31.java
+++ Panel_31.java
@@ -3,7 +3,7 @@ import javax.swing.*;
  
  /**
  * A panel that displays a name is created in this Class
- * Author : Srivan Reddy Gutha, sgutha3@asu.edu
+ * Author : Srivan Reddy Gutha, sgutha3@asu.edu ,GitID : 31
  */
  
 public class Panel_31 extends JPanel
@@ -15,29 +15,15 @@ public class Panel_31 extends JPanel
 	
 	private void initPanel()
     {
-	
-//	JFrame j_frame= new JFrame("Sample Panel");	//		To test the code for myself
-	
-	//JPanel j_panel=new JPanel();  
+	 
 	JLabel j_label = new JLabel();	
 
 	this.add(j_label);		
 	this.setBackground(Color.RED);		
-//	j_panel.setBounds(20,20,450,80);	
 	
 	j_label.setFont(new Font("Arial",1,20));
 	j_label.setText("Srivan Reddy");
 	
-/*
-	j_frame.add(j_panel);  
-	j_frame.setSize(750,450);
-    j_frame.setVisible(true);
-*/
 	}
-/*	
-	public static void main(String args[])
-    {
-        new Panel_31();		//		To test the code for myself
-    }
-*/
+
 }
