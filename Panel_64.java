e301b4c2ff55173e7bd2b853d5ee4f94ab881135
Jinal Patel <43081263+jspatel5@users.noreply.github.com>
2019-01-20 22:12:39 -0700

    New Panel created: Panel_64.java

---
+++ Panel_64.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+/**
+ *
+ * @author jinalPatel
+ * gitID: 64
+ */
+public class Panel_64 extends JPanel {
+   
+    Panel_64()  
+    {   
+	    JLabel jlabel = new JLabel("Jinal Patel");
+	    jlabel.setFont(new Font("Arial",1,20));
+	    setBounds(10,10,200,60);
+      setBackground(Color.Aquamarine);
+      setBorder(new LineBorder(Color.BLACK));
+      setVisible(true);
+      add(jlabel);	    
+    }
+}
