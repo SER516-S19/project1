48d37615d8aaf332cee35d65fd97da0660f2d669
surya23 <surya.cherukuri@admitkard.com>
2019-01-20 23:45:38 -0700

    commented Jframe

--- Panel_19.java
+++ Panel_19.java
@@ -3,24 +3,24 @@ import javax.swing.*;
 @SuppressWarnings("serial")
 public class Panel_19 extends JPanel {
     public Panel_19() {
-        initComponents();
+        Init();
     }
-    private void initComponents() 
+    private void Init() 
     {
     	
-    JFrame frame = new JFrame("Panel");
-    JPanel panel = new JPanel();
-    JLabel label = new JLabel("Surya Cherukuri");
+    //JFrame frame = new JFrame("Panel");
+    //JPanel panel = new JPanel();
+    JLabel label = new JLabel("Surya Cherukuri",JLabel.CENTER);
     
     label.setFont(new Font("TimesRoman",Font.BOLD,50));
     
-    panel.add(label);
+    this.add(label);
     //panel.setBounds(10,10,450,410);
-    panel.setBackground(Color.MAGENTA);
-    frame.add(panel);  
-    frame.setSize(500,500);    
-    frame.setLayout(null);
-    frame.setVisible(true);        
+    this.setBackground(Color.MAGENTA);
+//    frame.add(panel);  
+//    frame.setSize(500,500);    
+//    frame.setLayout(null);
+//    frame.setVisible(true);        
     }
     
     //public static void main(String args[])
