90f8947ecedcbe4066d8cd53d8539e3923425a93
HongfeiJu <hongfeiju@gmail.com>
2019-01-21 15:40:12 -0700

    Panel_35 remove trivial code

--- src/Panel_35.java
+++ src/Panel_35.java
@@ -33,15 +33,4 @@ public class Panel_35 extends JPanel {
 		if (testMode) System.out.println("assemble...");
 		add(new JLabel("Hongfei Ju"));
 	}
-
-
-	//testing
-/*	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		frame.setSize(500, 500);
-		frame.add(new Panel_35());
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-	}*/
-
 }
