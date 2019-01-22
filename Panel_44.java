ff3835990a6008fe96f1f38b40510719bf85632d
Koushik Kotamraju <kkotamra@asu.edu>
2019-01-21 18:36:09 -0700

    Removing borders

--- Panel_44.java
+++ Panel_44.java
@@ -19,25 +19,14 @@ class Panel_44 extends JPanel {
 
     /**
     * This method creates the panel with name label
-     * @param	coordinateX describes the X coordinate of the Panel boundary
-     * @param	coordinateY describes the Y coordinate of the Panel boundary
-     * @param	panelHeight describes the height of the Panel created.
-     * @param	panelWidth describes the width of the Panel boundary
      */
     private void init() {
-
-        int coordinateX = 10;
-        int coordinateY = 10;
-        int panelHeight = 60;
-        int panelWidth = 250;
         
-        this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         JLabel label = new JLabel("Koushik Kotamraju");
-        this.setLayout(new GridBagLayout());
+        this.setLayout(new BorderLayout());
         label.setFont(new Font("Papyrus",Font.PLAIN,15));
-        panel.setBounds(coordinateX, coordinateY, panelWidth, panelHeight);
         this.setVisible(true);
         this.setBackground(Color.LIGHT_GRAY);
-        this.add(label);
+        this.add(label);        
     }
 }
