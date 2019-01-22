99f3003d3317d821748b04902ed6b1c8cffa217d
Dhruv Patel <drpatel8@asu.edu>
2019-01-21 18:24:49 -0700

    Updated Panel_63

--- src/Panel_63.java
+++ src/Panel_63.java
@@ -1,8 +1,8 @@
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.border.Border;
 
 public class Panel_63 extends JPanel {
+
 	public Panel_63() {
 
 		createPanel();
@@ -10,16 +10,14 @@ public class Panel_63 extends JPanel {
 	}
 
 	private void createPanel(){
-		
-		JPanel panel = new JPanel();
-		panel.setBounds(10, 10, 250, 60);	//setting co-ordinates and size of the panel
-		panel.setBackground(Color.cyan);	//setting background color to cyan
-		panel.setBorder(BorderFactory.createLineBorder(Color.black, 1));	//setting border of the panel
-		
-		JLabel name = new JLabel("DHRUV PATEL"); 	//creating label with my name
-		name.setFont(new Font("Courier", Font.BOLD, 26));	//setting font to the label 
-		
-		panel.add(name);	//adding label into the panel
-		
+
+		JLabel name = new JLabel("Dhruv Patel"); 	//creating label with my name
+		name.setFont(new Font("Courier", Font.BOLD, 20));	//setting font to the label
+		add(name);
+		setBackground(Color.cyan);	//setting background color to cyan
+		setBorder(BorderFactory.createLineBorder(Color.black, 1));	//setting border of the panel
+
+
+
 	}
 }
