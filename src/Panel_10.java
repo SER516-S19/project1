d07fcad36a921230c539dcff45635ecbd9232903
AnkitaShivanand <43120970+AnkitaShivanand@users.noreply.github.com>
2019-01-21 20:09:32 -0700

    Update Panel_10.java

--- src/Panel_10.java
+++ src/Panel_10.java
@@ -14,18 +14,16 @@ public class Panel_10 extends JPanel {
 	}
 
 /* Creating the Panel*/
-	private JPanel getPanel(String name)
+	private void getPanel(String name)
 	{
-		JPanel jpanelObj = new JPanel();
+		//JPanel jpanelObj = new JPanel();
 		JLabel jlabelObj = new JLabel(name);
 		/* Set Font*/
 		jlabelObj.setFont(new Font("Calibri",10,50));
-		jpanelObj.add(jlabelObj);
-		/*Set Co-ordinates*/
-		jpanelObj.setBounds(30, 30, 70, 80);
+		add(jlabelObj);
 		/*Set background color*/
-		jpanelObj.setBackground(Color.GRAY);
-		return jpanelObj;
+		setBackground(Color.GRAY);
+		
 		
 	}
 
