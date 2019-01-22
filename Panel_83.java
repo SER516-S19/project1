fe0e33c711c2d32124813c08b19bfa100e25a30d
Bhavana <bvakkala@asu.edu>
2019-01-22 08:31:23 +0530

    Pushing Panel_83 java file, modified Tab_21

---
+++ Panel_83.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.BorderFactory;
+public class Panel_83 extends JPanel{
+   
+     public Panel_83(){
+
+    
+     JLabel jl = new JLabel("Bhavana");
+     Border bdr = BorderFactory.createLineBorder(Color.BLUE); 
+      jl.setBorder(bdr);
+	  this.setLayout(new GridBagLayout());
+      jl.setBounds(10,50,190,60);
+	   jl.setForeground(Color.BLUE);
+        this.setBackground(Color.WHITE);
+      this.add(jl);	
+    }
+   
+}
