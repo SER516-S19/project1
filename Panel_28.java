74a26b7ea383c523e9006aca46c573c93f9f138d
RajuKoushik <g.rajukoushik@gmail.com>
2019-01-21 22:21:25 -0700

    Update Panel_28.java

--- Panel_28.java
+++ Panel_28.java
@@ -1,26 +1,18 @@
-import java.awt.*;  
 import javax.swing.*;
+import java.awt.*;
+
 public class Panel_28 extends JPanel {
     public Panel_28() {
-        initializeComponents();
+        init();
     }
-    private void initializeComponents() 
-    {
-
-    JPanel p = new JPanel();
-    JLabel l = new JLabel("Raju Koushik Gorantla");
     
-    l.setFont(new Font("Papyrus",Font.PLAIN,15));
+    private void init() {
+        this.setLayout(new GridBagLayout());
+        JLabel nameLabel = new JLabel("Raju Koushik Gorantla");
+        this.add(nameLabel);
+        this.setBackground(Color.LIGHT_GRAY);
+        this.setVisible(true);
+    }
     
-    p.add(l);
-    p.setBounds(10,10,250,60);
-    p.setBackground(Color.LIGHT_GRAY);
-       
-    }
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
-    public static void main(String args[])
-    {
-        new Panel_28();
-    }
+   
 }
