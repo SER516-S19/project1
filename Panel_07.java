feb34748419acd8df377bb5603eed6929e144f64
Suraj7696 <42521193+Suraj7696@users.noreply.github.com>
2019-01-21 22:18:25 -0700

    Update Panel_07.java

--- Panel_07.java
+++ Panel_07.java
@@ -11,7 +11,7 @@ public class Panel_07 extends JPanel {
 	Panel_07() {
 		JLabel label = new JLabel();
 		label.setFont(new Font("Arial", 1, 20));
-		label.setText("Suraj");
+		label.setText("Suraj Atmakuri");
 		add(label);
 		setBorder(new LineBorder(Color.YELLOW));
 		setBounds(10, 10, 250, 60);
