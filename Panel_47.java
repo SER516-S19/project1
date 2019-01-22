d5e749d731eaa54f254c313e3abbea94951644e5
Cecilia <laplace95@gmail.com>
2019-01-21 22:49:46 -0700

    Debugging and testing

--- Panel_47.java
+++ Panel_47.java
@@ -10,16 +10,11 @@ import javax.swing.*;
 
 public class Panel_47 extends JPanel{
 	public Panel_47(){
-		JPanel panel_47 = new JPanel(new BorderLayout());
 		JLabel name = new JLabel("Cecilia La Place");
 		// Edits
 		name.setForeground(Color.RED);
 		name.setFont(new Font("Garamond", Font.PLAIN, 20));
-        panel_47.add(name, BorderLayout.CENTER);
-        panel_47.setBackground(Color.BLACK);
-	}
-
-	public static void main(String[] args){
-		new Panel_47();
+        this.add(name);
+        this.setBackground(Color.BLACK);
 	}
 }
