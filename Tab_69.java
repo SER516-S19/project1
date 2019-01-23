881beb640a786630315bd84949dd99ef669672a4
Jahnvi Rai <jrai4@asu.edu>
2019-01-23 01:43:33 -0700

    Checking thread Implementation

--- Tab_69.java
+++ Tab_69.java
@@ -4,9 +4,54 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.lang.reflect.Method;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
-public class Tab_69 extends JPanel {
+public class Tab_69 extends JPanel implements TabInterface {
+
+    @Override
+    public void startSayingHi() {
+//        Timer timer = new Timer();
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                // Your database code here
+//                Component panels =
+//
+//            }
+//        }, 2*60*1000);
+//
+        Thread t = new Thread() {
+                        @Override
+                        public void run() {
+
+                            for(int i =0;i<panelNames.length;i++) {
+                                try {
+                                    Class<?> clazz = Class.forName(panelNames[i]);
+                                    Method method = clazz.getMethod("sayHi",boolean.class);
+                                    Object panel = clazz.newInstance();
+                                    method.invoke(panel,true);
+                                    sleep(1000);
+                                    method.invoke(panel,false);
+                                }
+                                catch (Exception e){
+
+                                }
+                            }
+
+
+            }
+        };
+        t.start();
+
+    }
+
+    @Override
+    public void stopSayingHi() {
+
+    }
 
     //Method called from the main frame to access the tab name
     public String getName() {
@@ -24,6 +69,7 @@ public class Tab_69 extends JPanel {
             "Panel_89","Panel_62","Panel_37","Panel_66","Panel_72","Panel_67","Panel_64","Panel_75",
             "Panel_50", "Panel_XX","Panel_XX","Panel_XX","Panel_XX","Panel_XX"};
 
+    JPanel panel = new JPanel();
 
     // Default constructor
     Tab_69() {
@@ -31,6 +77,7 @@ public class Tab_69 extends JPanel {
         addPanel();
 
    }
+
     //Method to check for existing class panels and add them
     private void addPanel(){
         for(int i =0;i<panelNames.length;i++){
@@ -44,8 +91,11 @@ public class Tab_69 extends JPanel {
                 newPanel.add(new JLabel("Panel Missing " + panelNames[i]));
                 this.add(newPanel);
             }
-        }
 
+        }
     }
+
+
+
 
 }
