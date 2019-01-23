fcbdf0dd312448521c15e5bb7027b23c31cfef99
LAZZZZZZZy <wjyhz@hotmail.com>
2019-01-23 12:31:33 -0700

    Update Panel_87.java

--- Panel_87.java
+++ Panel_87.java
@@ -12,18 +12,33 @@ import javax.swing.SwingConstants;
  * @version 1.0
  */
 
-public class Panel_87 extends JPanel{
+public class Panel_87 extends JPanel implements PanelInterface{
 	public Panel_87() {
-		init();
-	}
-
-	private void init() {
 		JLabel name = new JLabel("Jiayan Wang");
 		name.setFont(new Font("Rage Italic",Font.BOLD, 30));
 		name.setForeground(Color.white);
 		name.setHorizontalAlignment(SwingConstants.CENTER);
 		this.setBackground(Color.BLACK);
 		this.setLayout(new BorderLayout());
-		this.add(name);
+		this.add(name,BorderLayout.CENTER);
 	}
+	
+	public void sayHi(boolean hi) {
+		JLabel Hi = new JLabel("Hi!");
+		Hi.setFont(new Font("Rage Italic",Font.BOLD, 30));
+		Hi.setForeground(Color.white);
+		Hi.setHorizontalAlignment(SwingConstants.CENTER);	
+		Hi.setVisible(false);
+		this.add(Hi,BorderLayout.SOUTH);
+		
+		if(hi) {
+			Hi.setVisible(true);
+			this.validate();
+		} else {
+			Hi.setVisible(false);
+			this.validate();
+		}
+			
+	}
+
 }
