134281a3a7b38f9d115972ef837067c97a8a87c4
sarthak-tiwari <43050725+sarthak-tiwari@users.noreply.github.com>
2019-01-22 23:40:53 -0700

    Added Interface files for all tabs and Name Panels

---
+++ PanelInterface.java
@@ -0,0 +1,13 @@
+
+/*  Class to create Interface which is to be used by all name Panels
+    Author: Sarthak Tiwari
+    E-Mail: sarthak-tiwari@asu.edu
+    Date:   1/22/2019
+*/
+
+public interface PanelInterface {
+    
+    //displays Hi if true is passed else hides it
+    public void sayHi(boolean flag);
+
+}
