1322cf12a3bbbab65d454c57dd7634781b27c599
virajtalaty <virajtalaty@gmail.com>
2019-01-23 13:01:46 -0700

    Implementing sayHi method

--- Panel_78.java
+++ Panel_78.java
@@ -2,25 +2,33 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 
-
 /* 
  * @author:  Viraj Talaty
  * @gitID:   78
  */
 
-
-public class Panel_78 extends JPanel
+public class Panel_78 extends JPanel implements PanelInterface
 {
-	
+	String panelName = "Viraj Talaty";
 	Panel_78()  
     {  
-	    
-	    JLabel jlabel = new JLabel("Viraj Talaty");
+	    JLabel jlabel = new JLabel(panelName);
 	    jlabel.setFont(new Font("Verdana",1,20));
-	    
         setBackground(Color.LIGHT_GRAY);
         setBorder(new LineBorder(Color.yellow));
         setVisible(true);
         add(jlabel);	    
     }
+	
+	@Override
+	public void sayHi(boolean flag) {
+		if(flag)
+		{
+			panelName = "Viraj Talaty, Hi!!";
+		}
+		else
+		{
+			panelName = "Viraj Talaty";
+		}
+	}
 }
