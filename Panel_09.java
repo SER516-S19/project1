309952dcfb30a172042853b7ca5916f528a53f0f
MayankBatra005 <37073720+MayankBatra005@users.noreply.github.com>
2019-01-24 13:48:32 -0700

    Update Panel_09.java

--- Panel_09.java
+++ Panel_09.java
@@ -9,37 +9,41 @@ import javax.swing.border.LineBorder;
  */
 
 @SuppressWarnings("serial")
-public class Panel_09 extends JPanel
+public class Panel_09 extends JPanel implements PanelInterface
 {
+	private JLabel jlabel;
 	Panel_09()  
     {  
- //   JFrame f= new JFrame("Panel Example");  //    For testing
-        // Created a panel
-//    JPanel panel=new JPanel();
-    // Created label
-    JLabel jlabel = new JLabel("Mayank ID 09");
+     jlabel = new JLabel("Mayank ID 09");
     // Font defined for label
-    jlabel.setFont(new Font("Verdana",1,20));
+ 	jlabel.setFont(new Font("Calibri",2,20));
+ 	
     this.add(jlabel);
-    // added label to panel
- //   panel.add(jlabel);
-    // boder and background defined for panel
- //   panel.setBorder(new LineBorder(Color.BLACK)); 
-    // panel.setBounds(10,10,250,60);    
-    // panel.setBackground(Color.WHITE);  
      
-//    f.add(panel);  
-//            f.setSize(600,600);    
-//            f.setLayout(null);    		//For Testing 
-//            f.setVisible(true);    
-// 
-    }  
+    }
 
-//	public static void main(String[] args)
-//	{
-//		new Panel_09();						//For Testing 
-//
-//	}
-
-
+	@Override
+	public void sayHi(boolean flag) 
+	{
+		// Public function to implement the Hi functionality with normal if else comparison 
+		
+		if (flag==true)
+		{
+			// if condition evaluates to true show hi at the end 
+			jlabel.setText("Mayank ID 09 HI ");
+		
+			   
+		}
+		else
+		{	// if condition is not true then show normal text
+			jlabel.setText("Mayank ID 09 ");
+			
+		}	 
+			 
+		
+	
+		}		
+	
 }
+
+
