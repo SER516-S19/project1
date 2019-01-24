58eae654f4560ccb95bc1176fa509ea90dc7c829
shefaliAJ7 <sanand22@asu.edu>
2019-01-23 17:40:28 -0700

    Added Hi functionality

--- Panel_06.java
+++ Panel_06.java
@@ -11,20 +11,36 @@ import javax.swing.border.BevelBorder;
 
 public class Panel_06 extends JPanel{
 	
-	Panel_06(){
-		init();
-	}
+	JLabel label_name;
+	JLabel label_hi;
 	
-	public void init() {
+	Panel_06(){
+		
 		this.setBackground(Color.PINK);
 		this.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+		this.setLayout(null);
  		
-		JLabel label1 = new JLabel("Shefali Anand", JLabel.CENTER);
-		label1.setFont(new Font("Comic Sans MS", Font.BOLD,30));
-		label1.setForeground(Color.BLUE);
+		label_name = new JLabel("Shefali Anand", JLabel.CENTER);
+		label_name.setFont(new Font("Comic Sans MS", Font.BOLD,30));
+		label_name.setForeground(Color.BLUE);
+		label_name.setBounds(5, 20, 300, 30);
 		
-		this.add(label1);
+		this.add(label_name);
 		
+		label_hi = new JLabel("Hi", JLabel.CENTER);
+		label_hi.setFont(new Font("Comic Sans MS", Font.BOLD,30));
+		label_hi.setForeground(Color.BLUE);
+		label_hi.setBounds(5, 60, 300, 30);
+		
+		this.add(label_hi);
+		
+	}
+
+	@Override
+	public void sayHi(boolean flag) {
+		
+		if(flag == false)
+			label_hi.setVisible(false);	
 	}
 	
 }
