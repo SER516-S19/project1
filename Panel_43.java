0a3626b99f044dbc1df9c35e80b2501e4b016c60
AravindSaiK <46664192+AravindSaiK@users.noreply.github.com>
2019-01-24 10:30:04 -0700

    Fixed Hi issue.

--- Panel_43.java
+++ Panel_43.java
@@ -4,22 +4,24 @@ import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author Aravinda Sai Kondamari
  * @GitId 43
  */
+
 public class Panel_43 extends JPanel implements PanelInterface{
 
+    JLabel labelHi;
+
     public Panel_43() {
-        JLabel labelName = new JLabel("Aravinda Sai Kondamari_43");
-        labelName.setFont(new Font("TimesRoman", Font.ITALIC, 14));
+        JLabel labelName = new JLabel("Aravinda Sai Kondamari");
+        labelName.setFont(new Font("TimesRoman", Font.ITALIC, 24));
         labelName.setHorizontalAlignment(JLabel.CENTER);
+        labelName.setVerticalAlignment(JLabel.CENTER);
+        labelHi = new JLabel("Hi");
+        labelHi.setFont(new Font("TimesRoman", Font.BOLD, 26));
+        labelHi.setHorizontalAlignment(JLabel.CENTER);
         setLayout(new BorderLayout());
         add(labelName);
         setBackground(Color.GREEN);
@@ -27,9 +29,6 @@ public class Panel_43 extends JPanel implements PanelInterface{
 
     @Override
     public void sayHi(boolean showHi) {
-        JLabel labelHi = new JLabel("Hi");
-        labelHi.setFont(new Font("TimesRoman", Font.BOLD, 16));
-        labelHi.setHorizontalAlignment(JLabel.CENTER);
           if (showHi) {
             add(labelHi, BorderLayout.SOUTH);
         } else {
