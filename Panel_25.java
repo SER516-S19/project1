7f0a0c758252a3dc7825a7879caccd44e11001c4
saivinayg <32012516+saivinayg@users.noreply.github.com>
2019-01-24 01:04:25 -0700

    Update Panel_25.java

--- Panel_25.java
+++ Panel_25.java
@@ -1,22 +1,32 @@
 import java.awt.*;  
 import javax.swing.border.LineBorder;
 import javax.swing.*;  
-public class Panel_25 extends JPanel {  
+public class Panel_25 extends JPanel implements PanelInterface{ 
+     JLabel l25 = new JLabel("SAI VINAY.G");
      Panel_25()  
         {  
    //     JFrame f25= new JFrame("Panel_25");    
-     //   JPanel p25=new JPanel(); 
+   //     JPanel p25= new JPanel();
         this.setBackground(Color.CYAN);
         this.setBounds(15, 30, 250, 250);
         this.setBorder(new LineBorder(Color.BLUE));
         this.setAlignmentX(CENTER_ALIGNMENT);
         this.setAlignmentY(CENTER_ALIGNMENT);
-        this.add(new JLabel("SAI VINAY. G"),new Font("Valencia",Font.HANGING_BASELINE,30));
+        //this.add(new JLabel("SAI VINAY. G"),new Font("Valencia",Font.HANGING_BASELINE,30));
+          this.add(l25);
+          
    /*    f25.add(this);  
                 f25.setSize(450,450);    
                 f25.setLayout(null);    
                 f25.setVisible(true);    */
-        }  
+        }
+     public void sayHi(boolean flag) {
+     	if(flag) {
+     		l25.setText("SAI VINAY. G" + "HI");
+     	}
+     	else {
+     		l25.setText("SAI VINAY. G");
+     	}
    /*    public static void main(String args[])  
         {  
         new Panel_25();  
