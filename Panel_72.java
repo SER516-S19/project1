21cefffa6f61a277d61e141fd67408a5d608b686
Alsha Samantaray <alsha1306.samantaray@gmail.com>
2019-01-23 19:08:27 -0700

    Implemented Hi functionality

--- Panel_72.java
+++ Panel_72.java
@@ -9,15 +9,31 @@ import java.awt.*;
  * @author Alsha Samantaray
  * gitID: 72
  */
- public class Panel_72 extends JPanel{
-
-     Panel_72(){
+ public class Panel_72 extends JPanel implements PanelInterface{
+	 
+	 Panel_72(){
          JLabel label = new JLabel("Alsha Samantaray",SwingConstants.CENTER);
          label.setVerticalAlignment(SwingConstants.CENTER);
-         label.setFont(new Font("Serif",Font.PLAIN,25));
+         Font labelFont = new Font("Serif",Font.PLAIN,25);
+         label.setFont(labelFont);
          setBorder(BorderFactory.createLineBorder(Color.black));
          setBackground(Color.GRAY);
          setVisible(true);
          add(label);
+        
      }
+	 
+	@Override
+	public void sayHi(boolean flag) {
+		JLabel panelLabel = (JLabel)getComponent(0);
+		if(flag)
+		{
+			panelLabel.setText("<html>Alsha Samantaray<br/>HI</html>");
+		}
+		else
+		{
+			panelLabel.setText("Alsha Samantaray");
+		}
+	}
+	
  }
