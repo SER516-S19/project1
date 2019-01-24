7166f3c471f2dbb05e7bc51542e792491ef780bc
xiangweizheng <43075724+xiangweizheng@users.noreply.github.com>
2019-01-24 13:15:29 -0700

    test fix hi issue on tab 48

--- Tab_48.java
+++ Tab_48.java
@@ -7,10 +7,10 @@ import java.awt.event.ActionListener;
 
 public class Tab_48 extends JPanel implements TabInterface {
 
-    private static String[] myTeamPanels = {"Panel_04", "Panel_10", "Panel_11",
+    private static String[] myTeamPanels = {"Panel_90","Panel_04", "Panel_10", "Panel_11",
             "Panel_23", "Panel_30", "Panel_35", "Panel_41", "Panel_43",
             "Panel_46", "Panel_56", "Panel_61", "Panel_63", "Panel_79",
-            "Panel_90", "", "", "", "", "", "",};
+             "", "", "", "", "", "",};
 
     String name = "Hsin-Jung Lee";
 
