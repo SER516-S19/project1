1502580228b1a0cdac70d52b8f5ece83e7f9f33c
Dhruv Patel <drpatel8@asu.edu>
2019-01-23 19:26:53 -0700

    Implemented PanelInterface to display Hi

--- Panel_63.java
+++ Panel_63.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Panel_63 extends JPanel {
+public class Panel_63 extends JPanel implements PanelInterface{
 
 	private String name = "Dhruv Patel";
 
@@ -9,12 +9,19 @@ public class Panel_63 extends JPanel {
 
 		JLabel label_63 = new JLabel(); 	//creating label with my name
 		label_63.setText(name);
-		label_63.setFont(new Font("Lucida Handwriting", Font.BOLD, 24));	//setting font to the label
+		label_63.setFont(new Font("Lucida Handwriting", Font.BOLD, 20));	//setting font to the label
 
 		add(label_63);
 		setLayout(new GridBagLayout());		//Align name to center
 		setBackground(Color.cyan);	//setting background color to cyan
 		setBorder(BorderFactory.createLineBorder(Color.black, 1));	//setting border of the panel
 
+	}
+
+	@Override
+	public void sayHi(boolean flag) {
+		if(flag){
+			name = "Dhruv Patel said Hi!!";
+		}
 	}
 }
