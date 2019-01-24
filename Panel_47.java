842896668c54bf03bc3cf52ef727795251908c0f
Cecilia <laplace95@gmail.com>
2019-01-23 20:56:05 -0700

    Adding functionality.

--- Panel_47.java
+++ Panel_47.java
@@ -8,13 +8,25 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Panel_47 extends JPanel{
+public class Panel_47 extends JPanel implements PanelInterface{
+	private JLabel greeting = new JLabel("HI");
+
 	public Panel_47(){
 		JLabel name = new JLabel("Cecilia La Place");
 		// Edits
 		name.setForeground(Color.RED);
+		greeting.setForeground(Color.RED);
+		greeting.setFont(new Font("Garamond", Font.BOLD, 20));
 		name.setFont(new Font("Garamond", Font.PLAIN, 20));
         this.add(name);
         this.setBackground(Color.BLACK);
+
+        this.add(greeting);
+	}
+
+	public void sayHi(boolean visible){
+		greeting.setVisible(visible);
+		this.repaint();
+
 	}
 }
