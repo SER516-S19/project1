e0de77c4abc034d07f514ebb56570890a9207194
Manikanta2 <42682212+Manikanta2@users.noreply.github.com>
2019-01-23 20:53:07 -0700

    Added new JLabel

--- Panel_17.java
+++ Panel_17.java
@@ -1,14 +1,26 @@
 import java.awt.*;  
 import javax.swing.*;
-public class Panel_17 extends JPanel {
-    public Panel_17() {
-        initComponents();
-    }
-    private void initComponents() 
+@SuppressWarnings("serial")
+public class Panel_17 extends JPanel implements PanelInterface {
+	private JLabel message_label;
+	private JLabel name_label;
+	
+    public Panel_17() 
     {
-	    JLabel label = new JLabel("Manikanta Chintakunta");   
-	    this.add(label);
-	    this.setBackground(Color.GRAY);	
-	    label.setFont(new Font("TimesRoman", Font.BOLD ,30));       
-    }
+    name_label = new JLabel("Manikanta Chintakunta");
+    name_label.setFont(new Font("TimesRoman", Font.BOLD ,25));
+	this.add(name_label);
+	
+	message_label = new JLabel("<html><br />Hi</html>",JLabel.CENTER);
+	message_label.setFont(new Font("TimesRoman", Font.BOLD ,15));
+	this.add(message_label);
+	this.setBackground(Color.GRAY);
+	message_label.setVisible(true);
+	}
+	public void sayHi(boolean flag){
+		this.message_label.setVisible(flag);
+	}
+	
+	
 }
+    
