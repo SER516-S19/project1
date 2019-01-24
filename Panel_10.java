9d44e2ffa5938a5bb41eca6eb44190bfbcf90f61
AnkitaShivanand <43120970+AnkitaShivanand@users.noreply.github.com>
2019-01-23 19:20:09 -0700

    #Id 10 Updated Panel_10

--- Panel_10.java
+++ Panel_10.java
@@ -9,7 +9,7 @@ public class Panel_10 extends JPanel implements PanelInterface {
 	/**
 	 * @author Ankita
 	 */
-	private String name = "Ankita Shivanand Bhandari";
+	private String name = "Ankita Shivanand";
 	private JLabel jlabelObj = new JLabel();
 	
 
