83f833466c958e461f4cd55614152177d53db7a2
MayankBatra005 <37073720+MayankBatra005@users.noreply.github.com>
2019-01-24 13:51:07 -0700

    Update Panel_09.java

--- Panel_09.java
+++ Panel_09.java
@@ -17,33 +17,23 @@ public class Panel_09 extends JPanel implements PanelInterface
      jlabel = new JLabel("Mayank ID 09");
     // Font defined for label
  	jlabel.setFont(new Font("Calibri",2,20));
- 	
     this.add(jlabel);
-     
     }
 
 	@Override
 	public void sayHi(boolean flag) 
 	{
 		// Public function to implement the Hi functionality with normal if else comparison 
-		
 		if (flag==true)
 		{
 			// if condition evaluates to true show hi at the end 
 			jlabel.setText("Mayank ID 09 HI ");
-		
-			   
 		}
 		else
 		{	// if condition is not true then show normal text
 			jlabel.setText("Mayank ID 09 ");
-			
 		}	 
-			 
-		
-	
 		}		
-	
 }
 
 
