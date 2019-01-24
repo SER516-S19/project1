dff1b3061e61f132fc35e32f092b63d312acac22
srivanreddy <43078002+srivanreddy@users.noreply.github.com>
2019-01-23 20:57:26 -0700

    Add files via upload
    Added new label for displaying Hi

--- Panel_31.java
+++ Panel_31.java
@@ -6,24 +6,34 @@ import javax.swing.*;
  * Author : Srivan Reddy Gutha, sgutha3@asu.edu ,GitID : 31
  */
  
-public class Panel_31 extends JPanel
+public class Panel_31 extends JPanel implements PanelInterface
 {
 	public Panel_31()
 	{
-        initPanel();
-    }
-	
-	private void initPanel()
-    {
-	 
-	JLabel j_label = new JLabel();	
 
-	this.add(j_label);		
+
+	this.add(j_label);
+	this.add(j_labelHi);			
+	
 	this.setBackground(Color.RED);		
 	
-	j_label.setFont(new Font("Arial",1,20));
-	j_label.setText("Srivan Reddy");
+	j_label.setFont(new Font("Arial",1,40));
+	j_label.setText("Srivan Reddy");	
+	
+	j_labelHi.setFont(new Font("Arial",1,40));
+	j_labelHi.setText("Hi");
+
+	JLabel j_label = new JLabel();
+	JLabel j_labelHi = new JLabel();
+
+	j_labelHi.setVisible(false);
 	
 	}
+
+	public void sayHi(boolean flag) {
+        this.j_labelHi.setVisible(flag);
+    }
+
+	private JLabel j_labelHi;
 
 }
