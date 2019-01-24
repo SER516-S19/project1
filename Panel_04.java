57db102d316e8a7ca3455e7f5ef2e78bd3c94749
Pradeep Ambalam Jawaharlal <pambalam@asu.edu>
2019-01-23 18:22:38 -0700

    ID #04: Implemented PanelInterface in Panel_04

--- Panel_04.java
+++ Panel_04.java
@@ -6,12 +6,23 @@ import javax.swing.*;
  * @version 1.0
  */
 
-class Panel_04 extends JPanel {
+class Panel_04 extends JPanel implements PanelInterface {
+    private JLabel label = new JLabel();
+    private String name = "Pradeep Ambalam Jawaharlal";
     public Panel_04() {
-        String name = "Pradeep Ambalam Jawaharlal";
-        JLabel label = new JLabel(name);
+        label.setText(name);
         label.setFont(new Font("Papyrus",Font.BOLD,20));
         add(label);
         setBackground(Color.LIGHT_GRAY);
     }
+
+    @Override
+	public void sayHi(boolean flag) {
+		if(flag) {
+			label.setText(name + " " + "HI");
+        } 
+        else {
+			label.setText(name);
+		}
+	}
 }
