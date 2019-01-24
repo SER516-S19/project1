7547cb2668f5fbd2d465e057c43910d1591d7eb5
hsinjlee <rlee4408@gmail.com>
2019-01-24 01:20:16 -0700

    add sayhi  method to avoid compiler error

--- Panel_90.java
+++ Panel_90.java
@@ -1,25 +1,24 @@
-import java.awt.*;
-
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Panel_90 creation
- * 
+ *
  * @author Xiangwei Zheng
  * @version 1.0
  */
 
 public class Panel_90 extends JPanel implements PanelInterface{
-	
+
 	//constructor
 	public Panel_90() {
 		initialPanel();
 	}
-	
+
 	//panel init
 	private void initialPanel() {
-		
-		
+
+
 		this.setLayout(new GridBagLayout());
                 this.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
                 JLabel nameLabel = new JLabel("Xiangwei Zheng");
@@ -27,8 +26,13 @@ public class Panel_90 extends JPanel implements PanelInterface{
                 this.add(nameLabel);
                 this.setBackground(Color.cyan);
                 this.setVisible(true);
-		
 
-		
+
+
+	}
+
+	@Override
+	public void sayHi(boolean flag) {
+
 	}
 }
