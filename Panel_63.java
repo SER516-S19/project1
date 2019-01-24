00b4567f19b1e8c45c9efb3113a018c7b469b581
Dhruv Patel <drpatel8@asu.edu>
2019-01-23 18:55:07 -0700

    Added panel creation in the constructor

--- Panel_63.java
+++ Panel_63.java
@@ -3,23 +3,18 @@ import javax.swing.*;
 
 public class Panel_63 extends JPanel {
 
+	private String name = "Dhruv Patel";
+
 	public Panel_63() {
 
-		createPanel();
-		
-	}
+		JLabel label_63 = new JLabel(); 	//creating label with my name
+		label_63.setText(name);
+		label_63.setFont(new Font("Lucida Handwriting", Font.BOLD, 24));	//setting font to the label
 
-	private void createPanel(){
-
-		JLabel name = new JLabel("Dhruv Patel"); 	//creating label with my name
-		name.setFont(new Font("Courier", Font.BOLD, 20));	//setting font to the label
-
-		add(name);
+		add(label_63);
 		setLayout(new GridBagLayout());		//Align name to center
 		setBackground(Color.cyan);	//setting background color to cyan
 		setBorder(BorderFactory.createLineBorder(Color.black, 1));	//setting border of the panel
-
-
 
 	}
 }
