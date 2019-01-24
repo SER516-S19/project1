3f8fe9a617b1de4641ce2aeddf1c2413fe64f5f0
Sajith Thattazhi <sthattaz@asu.edu>
2019-01-23 19:57:43 -0700

    Panel_80 - Correction of Interface implementation

--- Panel_80.java
+++ Panel_80.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Panel_80 extends JPanel implements TabInterface
+public class Panel_80 extends JPanel implements PanelInterface
 {
     public Panel_80()
 	{
