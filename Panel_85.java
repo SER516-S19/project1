8fc4fdcf38acc44b53dc21b7c993258262673bbc
Shivam Verma <shivamv2392@gmail.com>
2019-01-24 01:46:42 -0700

    Added sayHi function of PanelInterface

--- Panel_85.java
+++ Panel_85.java
@@ -6,7 +6,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Panel_85 extends JPanel {
+public class Panel_85 extends JPanel implements PanelInterface {
 
 	public Panel_85() {
 		initPanel();
@@ -17,4 +17,9 @@ public class Panel_85 extends JPanel {
         this.setBackground(Color.LIGHT_GRAY);	
         this.add(label);
     }
+
+    @Override
+	public void sayHi(boolean flag) {
+		this.timerLabel.setVisible(flag);
+	}
 }
