e3112532d322ead2251f1d7507ba7d2c3742b765
schaud28 <schaud28@asu.edu>
2019-01-23 22:53:13 -0700

    Implemented sayHi method in Panel_14

--- Panel_14.java
+++ Panel_14.java
@@ -1,21 +1,36 @@
 /* 
  * @author: Sakshi Chaudhary	 
- * @gitID: 14 
+ * @gitID: 14
  */
 
 import java.awt.*;  
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 
-public class Panel_14 extends JPanel
+public class Panel_14 extends JPanel implements PanelInterface
 {
   Panel_14()  
    {  
-     JLabel mylabel = new JLabel("Sakshi Chaudhary");
+     JLabel mylabel = new JLabel();
+     mylabel.setText("Sakshi Chaudhary");
      mylabel.setFont(new Font("Calibri",1,20)); 
      setBorder(new LineBorder(Color.black));
      setBackground(Color.pink);
      setVisible(true);
      add(mylabel);	    
     }
+
+   @override
+   public void sayHi(boolean flag)
+   { 
+     JLabel mylabel = (JLabel)getComponent(0);
+     if(flag)
+     {
+	     mylabel.setText("<html>Sakshi Chaudhary<br/>Hi!</html>");
+     }
+     else
+     {
+	     mylabel.setText("Sakshi Chaudhary");
+     }
+   }	
 }
