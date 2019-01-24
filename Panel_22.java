2a3dd6af6ce1c4e2d48aaa4359d79ec138478172
venkata sai ram <veadala@asu.edu>
2019-01-23 22:01:04 -0700

    Implementing panelinterface to respond hi

--- Panel_22.java
+++ Panel_22.java
@@ -6,23 +6,33 @@ This Panel belongs to Tab Jashua Drumm.
 Author: Sai Ram Eadala
 ----------------------------*/
 
-@SuppressWarnings("serial")
-public class Panel_22 extends JPanel {
 
-    public Panel_22() {
-       initComponents();
-   }
+@SuppressWarnings("serial")
+public class Panel_22 extends JPanel implements PanelInterface{
+	JLabel label;
+	String Name="VenkataSairamEadala";
     
-    private void initComponents() {
-        
-       this.setLayout(new GridBagLayout());
-        JLabel label = new JLabel("SaiRam Eadala");
-        label.setAlignmentX(CENTER_ALIGNMENT);
-        label.setFont(new Font("Italic", Font.ITALIC, 18));
+	public Panel_22() {
+        this.setLayout(new GridBagLayout());
+       label = new JLabel(Name);
+        label.setFont(new Font("Italic", Font.ITALIC, 20));
         label.setForeground(Color.LIGHT_GRAY);
         this.setBackground(Color.WHITE);
         this.add(label);
-        this.setAlignmentX(CENTER_ALIGNMENT);
+       
+}
+    
+    public void sayHi(boolean flag) {
+
+    if(flag) {
+
+        this.label.setText(Name);
+
+    }else {
+
+        this.label.setText("<html>"+Name+"<br>"+"HI"+"</html>");
+
+    }
 
 }
 }
