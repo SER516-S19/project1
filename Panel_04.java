fcfcb14def30561f1b80517cd483e4f65e620f91
Pradeep Ambalam Jawaharlal <pambalam@asu.edu>
2019-01-23 19:36:38 -0700

    ID #04: update panel_04

--- Panel_04.java
+++ Panel_04.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 
 class Panel_04 extends JPanel implements PanelInterface {
     private JLabel label = new JLabel();
-    private String name = "Pradeep Ambalam Jawaharlal";
+    private String name = "Pradeep AJ";
     public Panel_04() {
         label.setText(name);
         label.setFont(new Font("Papyrus",Font.BOLD,20));
