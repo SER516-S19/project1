bd56853fe7d8dcefc0847d1e7c449721c935f74d
raghava  kannikanti <raghavakannikanti@raghavas-MacBook-Air.local>
2019-01-23 18:30:17 -0700

    Add sayHi implementation

--- Panel_65.java
+++ Panel_65.java
@@ -4,26 +4,36 @@ import java.awt.*;
 /**
  * Panel class for GIT ID - 65
  */
-public class Panel_65 extends JPanel {
+public class Panel_65 extends JPanel implements PanelInterface {
 
+    private Container container;
 
-    public Panel_65(){
-        createPanel();
+    public Panel_65() {
+        container = Box.createVerticalBox();
+        container.setBackground(Color.lightGray);
+        container.add(createJLabel());
+        this.add(container);
     }
 
-
-    public void createPanel(){
-        this.setBackground(Color.lightGray);
-        this.add(createJLabel());
+    private JLabel createJLabel() {
+        JLabel myJLabel = new JLabel();
+        myJLabel.setText("Lakshmi Kala Pedarla");
+        myJLabel.setFont(new Font("monaco", Font.BOLD, 24));
+        return myJLabel;
     }
 
+    @Override
+    public void sayHi(boolean flag) {
+        JLabel myJLabel = new JLabel();
+        myJLabel.setText("         Hi");
+        myJLabel.setForeground(Color.BLUE);
+        myJLabel.setFont(new Font("monaco", Font.BOLD, 24));
+        myJLabel.setVisible(true);
 
-    private JLabel createJLabel(){
-        JLabel jLabel = new JLabel();
-        jLabel.setText("Lakshmi Kala Pedarla");
-        jLabel.setFont(new Font("monaco", Font.BOLD, 24));
-        return jLabel;
+        if (flag) {
+            myJLabel.setVisible(true);
+            container.add(myJLabel);
+            container.setBackground(Color.lightGray);
+        }
     }
-
-
 }
