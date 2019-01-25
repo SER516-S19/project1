4da27dee64fab7bfdf4e4947a0935fbbc202669f
Jinal Patel <43081263+jspatel5@users.noreply.github.com>
2019-01-24 22:13:37 -0700

    Adding Java Doc Comments in the file.

--- Panel_64.java
+++ Panel_64.java
@@ -3,37 +3,37 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 
 /**
+ * This Class is adding user name in Panel allocated in Tab5
+ * and adds Hi according to passed timer flag.
  *
- * @author jinalPatel
+ * @author Jinal Patel
+ * @version 1.1
+ * @since 01/22/2019
  * @gitUserID : jspatel5
- * gitID: 64
+ * @gitID: 64
  */
-
 public class Panel_64 extends JPanel implements PanelInterface{
-	
-    Panel_64()  
-    {   
-	    JLabel jlabel = new JLabel();
-	    jlabel.setText("Jinal Patel");
-	    jlabel.setFont(new Font("Brush Script MT",1,20));
-	    setBounds(10,10,200,60);
-      setBackground(Color.YELLOW);
-      setBorder(new LineBorder(Color.BLACK));
-      setVisible(true);
-      add(jlabel);	    
-    }
-    
+
+	//Constructor to initialize Panel attributes
+	Panel_64(){   
+		JLabel userNameLabel = new JLabel();
+		userNameLabel.setText("Jinal Patel");
+		userNameLabel.setFont(new Font("Brush Script MT",1,20));
+		setBounds(10,10,200,60);
+		setBackground(Color.YELLOW);
+		setBorder(new LineBorder(Color.BLACK));
+		setVisible(true);
+		add(userNameLabel);	    
+	}
+
 	@Override
 	public void sayHi(boolean flag) {
-		JLabel jlabel = (JLabel)getComponent(0);
-		if(flag)
-		{
-			jlabel.setText("<html>Jinal Patel<br/>Hi!</html>");
-		}
-		else
-		{
-			jlabel.setText("Jinal Patel");
+		JLabel userNameLabel = (JLabel)getComponent(0);
+		if(flag){
+			userNameLabel.setText("<html>Jinal Patel<br/>Hi!</html>");
+		}else{
+			userNameLabel.setText("Jinal Patel");
 		}
 	}
-	
+
 }
