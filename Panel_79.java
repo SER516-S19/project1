816a30c634ab87ae017c8ab533034c76326bbd8f
Aprajita Thakur <athaku20@asu.edu>
2019-01-24 23:19:46 -0700

    Refactor the code for Panel_79

--- Panel_79.java
+++ Panel_79.java
@@ -1,17 +1,34 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Panel_79 extends JPanel implements PanelInterface{
+/**
+* For SER 516 
+* @author Aprajita Thakur
+* @version 1.0
+* @since 01-17-2019
+*/
+
+public class Panel_79 extends JPanel implements PanelInterface
+{
     private String name = "Aprajita Thakur";
     private JLabel label = new JLabel();
-    public Panel_79() {
+    
+    public Panel_79() 
+    {
         label.setText(name);
         label.setFont(new Font("Papyrus",Font.BOLD,20));
         add(label);
         setBackground(Color.GREEN);
     }
+    /**
+    * Display "HI" with label name on the Panel. 
+    *
+    * @param  flag   Checking for true/false conditions 
+    * 
+    */
     @Override
-    public void sayHi(boolean flag){
+    public void sayHi(boolean flag)
+    {
         if (flag){
             label.setText(name + " "+ "HI");
         }
