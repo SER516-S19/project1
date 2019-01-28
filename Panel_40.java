cae18ae300f5a16bfc7abf8354aa62149555ad71
Trupti Khatavkar <khatavkar.trupti@gmail.com>
2019-01-28 13:43:15 -0700

    Panel 40 Clean code Update

--- Panel_40.java
+++ Panel_40.java
@@ -1,27 +1,35 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagLayout;
 
-public class Panel_40 extends JPanel implements PanelInterface{
-   
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * 
+ * @author Trupti Khatavkar GitID 40
+ *
+ */
+
+public class Panel_40 extends JPanel implements PanelInterface {
+
 	JLabel nameLabel;
-	String panelText;
+
 	public Panel_40() {
-    	this.setLayout(new GridBagLayout());
-        nameLabel = new JLabel("Trupti Khatavkar");
-        nameLabel.setFont(new Font("Times New Roman", Font.BOLD,20));
-        this.add(nameLabel);
-        this.setBackground(Color.yellow);
-        this.setVisible(true);
-    }
-    
-   
-    @Override
-    public void sayHi(boolean flag)
-    {
-        if(flag)
-            this.nameLabel.setText("<html><center>Trupti Khatavkar<br>HI</center></html>");
-        else
-            this.nameLabel.setText("Trupti Khatavkar");
-    }
-   
+		this.setLayout(new GridBagLayout());
+		nameLabel = new JLabel("Trupti Khatavkar");
+		nameLabel.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		this.add(nameLabel);
+		this.setBackground(Color.yellow);
+		this.setVisible(true);
+	}
+
+	@Override
+	public void sayHi(boolean flag) {
+		if (flag)
+			this.nameLabel.setText("<html><center>Trupti Khatavkar<br>HI</center></html>");
+		else
+			this.nameLabel.setText("<html><center>Trupti Khatavkar</center></html>");
+	}
+
 }
