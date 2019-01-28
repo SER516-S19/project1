7601d9820b36fc175ab84bc51d85834699826782
Darshan Prakash <darshanprakash.rvce@gmail.com>
2019-01-28 13:04:27 -0700

    updated the code with alignment

--- Panel_66.java
+++ Panel_66.java
@@ -1,48 +1,37 @@
-import java.awt.Color;
-import java.awt.Dimension;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.*;
 import javax.swing.border.LineBorder;
 
 /**
- * Panel_66() creates panel 66 for tab 5 in this application
- * The panel displays the Full name of the author
- * 
+ * The panel displays the Full name of the author in tab 5 of this app.
+ *
  * @author $Darshan Prakash
+ * @gitID: 66
  */
 
-public class Panel_66 extends JPanel implements PanelInterface {
-	
+public class Panel_66 extends JPanel implements PanelInterface {	
 	public Panel_66() {
-		
 		createPanel_66();
 	}
 	
-	private void createPanel_66() {
-		
-	JLabel label = new JLabel();
-	label.setText("Darshan Prakash");
-	label.setForeground(new Color(255, 198, 39));
-	setBackground(new Color(140, 29, 64));
-	setBorder(new LineBorder(new Color(255, 198, 39)));
-	setVisible(true);
-        add(label);
-
+	private void createPanel_66() {	
+		JLabel label = new JLabel();
+		label.setText("Darshan Prakash");
+		label.setForeground(new Color(255, 198, 39));
+		setBackground(new Color(140, 29, 64));
+		setBorder(new LineBorder(new Color(255, 198, 39)));
+		setVisible(true);
+        	add(label);
 	}
 	
 	@Override
 	public void sayHi(boolean flag) {
 		JLabel label = (JLabel)getComponent(0);
-		if(flag)
-		{
+		if(flag) {
 			label.setText("<html>Darshan Prakash<br/>Hi!</html>");
 		}
-		else
-		{
+		else {
 			label.setText("Darshan Prakash");
 		}
 	}
-
 }
