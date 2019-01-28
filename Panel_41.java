421ea64d531c08ff60649b6485db3bcfd427e3e5
HarikaKolli <42759081+HarikaKolli@users.noreply.github.com>
2019-01-27 19:13:19 -0700

    Panel_41 has been refactored to maintain the code standards.

--- Panel_41.java
+++ Panel_41.java
@@ -1,36 +1,48 @@
 import java.awt.*;
 import javax.swing.*;
 
-class Panel_41 extends JPanel implements PanelInterface {
+/**
+ * Creation of Panel_41 with formatted name
+ * @author Harika Kolli
+ * @version 3.0
+ */
 
+class Panel_41 extends JPanel implements PanelInterface 
+{
 	private JLabel label = new JLabel();
 	private String panel_name = "Harika Kolli";
 	
-    public Panel_41() {
-        panel41();
-    }
-
-    private void panel41() {
-    	
-       // JFrame jFrame = new JFrame();
-    	label.setText(panel_name);
-
-        /* SET THE FONT STYLE AND SIZE FOR THE GIVEN TEXT IN THE PANEL */
-        label.setFont(new Font("Calibri",Font.ITALIC,18));
-        add(label);
-        setBackground(Color.YELLOW);
-    }
-    @Override
-    public void sayHi(boolean flag)
-    {
-    	if(flag)
-    	{
-    		label.setText(panel_name+ " HI");
-    	}
-    	else
+	/**
+    	*  Panel_41 constructor to create and style the panel 
+    	*/	
+    
+    	public Panel_41() 
     	{
     		label.setText(panel_name);
+        	label.setFont(new Font("Calibri",Font.ITALIC,18));
+        	add(label);
+        	setBackground(Color.YELLOW);
     	}
-    	
-    }
+	
+	/**
+    	* Display name with "HI" based on flag. 
+    	*
+    	* @param flag, To display name with HI or not
+    	* flag = true wil display name with HI
+    	* flag = false will display just the name 
+   	* 
+    	*/
+    
+   	@Override
+    	public void sayHi(boolean flag)
+    	{
+    		if(flag)
+    		{
+    			label.setText(panel_name+ " HI");
+    		}
+    		else
+    		{
+    			label.setText(panel_name);
+    		}	
+    	}
 }
