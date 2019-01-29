c007354e8a8987a9cb57607a1a8853fa42d71d83
Abhishek Gupta <cruderock@gmail.com>
2019-01-28 21:24:22 -0700

    Clean Code. Adding comments.

--- Panel_29.java
+++ Panel_29.java
@@ -6,23 +6,31 @@ import java.awt.*;
  * Created: 1/19/2019
  */
 public class Panel_29 extends JPanel implements PanelInterface {
-    private JLabel label = new JLabel();
-    private String name = "Abhishek Gupta";
+    private JLabel PanelLabel = new JLabel();
+    private String UserName = "Abhishek Gupta";
+	
+    /**
+    * Class constructor.
+    */
     public Panel_29() {
         setLayout(new GridBagLayout());
-        label.setText(name);
-        add(label);
+        PanelLabel.setText(UserName);
+        add(PanelLabel);
         setBackground(Color.lightGray);
         setVisible(true);
     }
     
+    /**
+    * This method has been overridden from the interface toggles
+    * the text on the panel to show Hi when the flag is true.
+    */
     @Override
 	public void sayHi(boolean flag) {
 		if(flag) {
-            label.setText(name + " " + "HI");
+            label.setText(UserName + " " + "HI");
         } 
         else {
-			label.setText(name);
+			label.setText(UserName);
 		}
 	}
     
