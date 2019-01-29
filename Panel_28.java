8b9adb0e59a568d796c0fdd930288bcb912dcad1
RajuKoushik <g.rajukoushik@gmail.com>
2019-01-28 20:57:51 -0700

    Update Panel_28.
    Cleaned the code.

--- Panel_28.java
+++ Panel_28.java
@@ -1,29 +1,38 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**
+* SER-516 Software Agility
+* Panel_28 that displays name Label along with Hi Label
+* @author Raju Koushik Gorantla, rkgorant@asu.edu
+* @version 1.3
+* @since 01-18-19
+*/
+
+
+@SuppressWarnings("serial")
 public class Panel_28 extends JPanel implements PanelInterface {
-    
-    JLabel nameLabel;
-	String panelText;
-    public Panel_28() {
-        
-        this.setLayout(new GridBagLayout());
-        nameLabel = new JLabel("Raju Koushik Gorantla");
-        nameLabel.setFont(new Font("Arial", Font.BOLD,25));
-        this.add(nameLabel);
-        this.setBackground(Color.LIGHT_GRAY);
-        this.setVisible(true);
-        
-    }
-    
-    @Override
-    public void sayHi(boolean flag)
+	private JLabel message_label;
+	private JLabel name_label;
+	
+    public Panel_28() 
     {
-        if(flag)
-            this.nameLabel.setText("<html><center>Raju Koushik Gorantla<br>HI</center></html>");
-        else
-            this.nameLabel.setText("Raju Koushik Gorantla");
+    	name_label = new JLabel("Raju Koushik Gorantla");
+    	name_label.setFont(new Font("TimesRoman", Font.BOLD ,25));
+	this.add(name_label);
+	message_label = new JLabel("<html><br />Hi !</html>",JLabel.CENTER);
+	message_label.setFont(new Font("TimesRoman", Font.BOLD ,15));
+	this.add(message_label);
+	this.setBackground(ColorLIGHT_GRAY);
+	message_label.setVisible(false);
     }
-    
-   
+    /**
+    * Visibility of the Hi Label is set using a flag
+    * @param flag holds the value either true or false
+    */
+    public void sayHi(boolean flag){
+	this.message_label.setVisible(flag);
+    }	
 }
+
+
