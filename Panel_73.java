0dbae023957bd0408d1398b09dd9789bc756af58
Gamy Interactive <gamyinteractive@gamys-mbp.3500.dhcp.asu.edu>
2019-01-29 13:59:39 -0700

    changes according to best practices in coding

--- Panel_73.java
+++ Panel_73.java
@@ -3,38 +3,37 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 
 
-/*
- * @author:  Muhammad Sami
-    GIT Id: 73
- * Creating panel 73
- */
+/*  The below class implements Panel 73
+*
+* @author : Muhammad Sami
+* @version : 2.0
+* @GitId : 73
+*/
 
 
-public class Panel_73 extends JPanel implements PanelInterface
-{
-	
-	
-    Panel_73()  
-    {  
+public class Panel_73 extends JPanel implements PanelInterface {
+	private javax.swing.JLabel labelName;
 
-      JLabel jlabel = new JLabel("Muhammad Sami");
-      jlabel.setFont(new Font("Arial", Font.PLAIN, 20));
+	/* Constructor for panel */
+    Panel_73() {  
+
+      labelName = new JLabel("Muhammad Sami");
+      labelName.setFont(new Font("Arial", Font.PLAIN, 20));
       setBackground(Color.LIGHT_GRAY);
       setBorder(new LineBorder(Color.BLACK));
       setVisible(true);
-      add(jlabel);
+      this.add(labelName);
     }
+    
+    
 	@Override
-	public void sayHi(boolean flag)
-	{
-		JLabel jlabel=(JLabel)getComponent(0);
-		if(flag)
-		{
-			jlabel.setText("<html>Muhammad Sami<br/>Hi!</html>");
+	public void sayHi(boolean flag) {
+		JLabel labelMessage =(JLabel)getComponent(0);
+		if(flag) {
+			labelMessage.setText("<html>Muhammad Sami<br/>Hi!</html>");
 		}
-		else
-		{
-			jlabel.setText("Muhammad Sami");
+		else {
+			labelMessage.setText("Muhammad Sami");
 		}
 		
 	}
