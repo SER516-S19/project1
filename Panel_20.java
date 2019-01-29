0f5b19f6cb31822a859a68ea19d445fb836e3bbd
yrdesai <43055323+yrdesai@users.noreply.github.com>
2019-01-29 13:47:49 -0700

    Updated PANEL_20

--- Panel_20.java
+++ Panel_20.java
@@ -10,22 +10,22 @@ import javax.swing.*;
 
 public class Panel_20 extends JPanel implements PanelInterface {
 	
-	JLabel l1=new JLabel("Yuti Desai",JLabel.CENTER); 	
-	JLabel l2=new JLabel("Hi",JLabel.CENTER); 
+	JLabel labelName=new JLabel("Yuti Desai",JLabel.CENTER); 	
+	JLabel labelHi=new JLabel("Hi",JLabel.CENTER); 
 	
 	public Panel_20()  
     	{		
-		l1.setBounds(100,100,100,100);  
-		l1.setForeground(Color.red); 
-		l1.setFont(new Font("Courier",Font.BOLD, 40));	
-		l2.setBounds(100,100,100,100);  
-		l2.setForeground(Color.red); 
-		l2.setFont(new Font("Courier",Font.BOLD, 40));	
+		labelName.setBounds(100,100,100,100);  
+		labelName.setForeground(Color.red); 
+		labelName.setFont(new Font("Courier",Font.BOLD, 40));	
+		labelHi.setBounds(100,100,100,100);  
+		labelHi.setForeground(Color.red); 
+		labelHi.setFont(new Font("Courier",Font.BOLD, 40));	
 		this.setPreferredSize(new Dimension(300, 300));
 	    	this.setBackground(Color.orange);	
-	    	this.add(l1);
-	    	this.add(l2);
-	    	l2.setVisible(false);
+	    	this.add(labelName);
+	    	this.add(labelHi);
+	    	labelHi.setVisible(false);
 	 
     	}  
 	
@@ -33,6 +33,6 @@ public class Panel_20 extends JPanel implements PanelInterface {
 	@Override
 	public void sayHi(boolean flag)
     	{ 
-		l2.setVisible(flag);
+		labelHi.setVisible(flag);
     	} 
 }
