3db533db85a4faadbe5452e7163eddda73133e17
vbhavana <bhavanavakkalagadda@gmail.com>
2019-01-30 02:10:35 +0530

    Update Panel_83.java

--- Panel_83.java
+++ Panel_83.java
@@ -2,28 +2,45 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.BorderFactory;
-public class Panel_83 extends JPanel implements PanelInterface{
-   JLabel jl ;
-     public Panel_83(){
 
-    
-      jl = new JLabel("Bhavana");
-     Border bdr = BorderFactory.createLineBorder(Color.BLUE); 
-      jl.setBorder(bdr);
-	  this.setLayout(new GridBagLayout());
-      jl.setBounds(10,50,190,60);
-	   jl.setForeground(Color.BLUE);
-        this.setBackground(Color.WHITE);
-      this.add(jl);	
+
+/**
+ * --------------------------
+ * 
+ * @author Bhavana
+ * This Panel belongs to Tab Jashua Drumm.(Tab 21)
+ *  ----------------------------
+ */
+
+public class Panel_83 extends JPanel implements PanelInterface{
+       JLabel jLbl ;
+       String Name = "Bhavana";
+
+       public Panel_83()
+    {
+       jLbl = new JLabel(Name);
+       Border bdr = BorderFactory.createLineBorder(Color.BLUE); 
+       jLbl.setBorder(bdr);
+       this.setLayout(new GridBagLayout());
+       jLbl.setBounds(10,50,190,60);
+       jLbl.setForeground(Color.BLUE);
+       this.setBackground(Color.WHITE);
+       this.add(jl);	
     }
+
 	@Override
-	 public void sayHi(boolean flag) {
-     if(flag){
-       jl.setText("Bhavana" + " Hi");
-     }
-     else {
-       jl.setText("Bhavana");
-     }
-   }
+	 public void sayHi(boolean flag)
+      {
+              if(flag)
+              {
+               //Aligning "HI"
+               jLbl.setText(Name + " Hi");
+              }
+              else 
+              {
+              jLbl.setText(Name);
+              }
+       }
    
 }
+
