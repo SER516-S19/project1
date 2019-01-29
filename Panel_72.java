fcb2b70cfad3f263406ef35cd310e1491f61765d
Alsha Samantaray <alsha1306.samantaray@gmail.com>
2019-01-28 19:39:48 -0700

    Updated comments and cleaned the code

--- Panel_72.java
+++ Panel_72.java
@@ -5,26 +5,33 @@ import javax.swing.BorderFactory;
 import java.awt.*;
 
 /**
+ *This class creates a panel and sets the visible text.
  *
  * @author Alsha Samantaray
  * gitID: 72
  */
  public class Panel_72 extends JPanel implements PanelInterface{
 	 
-	 Panel_72(){
-         JLabel label = new JLabel("Alsha Samantaray",SwingConstants.CENTER);
-         label.setVerticalAlignment(SwingConstants.CENTER);
-         Font labelFont = new Font("Serif",Font.PLAIN,25);
-         label.setFont(labelFont);
+	 Panel_72()
+	 {
+         JLabel panelText = new JLabel("Alsha Samantaray",SwingConstants.CENTER);
+         panelText.setVerticalAlignment(SwingConstants.CENTER);
+         int fontSize = 25;
+         Font labelFont = new Font("Serif",Font.PLAIN,fontSize);
+         panelText.setFont(labelFont);
          setBorder(BorderFactory.createLineBorder(Color.black));
          setBackground(Color.GRAY);
          setVisible(true);
-         add(label);
+         add(panelText);
         
      }
 	 
+	 /**
+	  * @param flag sets Hi as the panel text when true
+	  */
 	@Override
-	public void sayHi(boolean flag) {
+	public void sayHi(boolean flag) 
+	{
 		JLabel panelLabel = (JLabel)getComponent(0);
 		if(flag)
 		{
