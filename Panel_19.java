31d0350f50ae8c160182fc7cdd18e9139aebc9f1
aneeshdalvi <adalvi1@asu.edu>
2019-01-28 19:38:27 -0700

    Cleaning code and adding important comments

--- Panel_19.java
+++ Panel_19.java
@@ -5,14 +5,19 @@ import javax.swing.border.LineBorder;
 
 /*
  * @author: Aneesh Dalvi
- * @ID: 19
+ * @gitID: 19
  */
 
-
+/**
+ *  Creates a panel and adds a label with the user's name
+ */
 public class Panel_19 extends JPanel implements PanelInterface
 {
 
-
+    /**
+    *   Constructor used to create and add a label with the user
+    *   name and assign the label properties to Panel_19
+    */
     Panel_19()
     {
 
@@ -27,11 +32,17 @@ public class Panel_19 extends JPanel implements PanelInterface
 
     }
 
+    /**
+    *   When the hiFlag is set to true, the panel_19 shows
+    *   the user name along with a Hi! message.
+    *   If the hiFlag is false, then only the user name is shown.
+    *   @param hiFlag
+    */
     @Override
-    public void sayHi(boolean flag)
+    public void sayHi(boolean hiFlag)
     {
         JLabel labelName = (JLabel)getComponent(0);
-        if(flag)
+        if(hiFlag)
         {
             labelName.setText("<html>Aneesh Dalvi<br/>Hi!</html>");
         }
