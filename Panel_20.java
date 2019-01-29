0e8f44ddbf6a2c213f01d371f9bcba637d90e383
Yuti Desai <yrdesai@asu.edu>
2019-01-29 13:35:37 -0700

    PANEL_20: cleaned the code

--- Panel_20.java
+++ Panel_20.java
@@ -1,18 +1,20 @@
 import java.awt.*;  
 import javax.swing.*; 
 
+/**
+ * Author: Yuti Desai
+ * GitID: 20
+ * GitUser: yrdesai
+ * Created Date: 19 Jan 2019
+ */
+
 public class Panel_20 extends JPanel implements PanelInterface {
 	
+	JLabel l1=new JLabel("Yuti Desai",JLabel.CENTER); 	
 	JLabel l2=new JLabel("Hi",JLabel.CENTER); 
 	
 	public Panel_20()  
-    {  
-		mypanel();
-    }
-	
-	public void mypanel()
-	{
-		JLabel l1=new JLabel("Yuti Desai",JLabel.CENTER); 		
+    	{		
 		l1.setBounds(100,100,100,100);  
 		l1.setForeground(Color.red); 
 		l1.setFont(new Font("Courier",Font.BOLD, 40));	
@@ -20,15 +22,17 @@ public class Panel_20 extends JPanel implements PanelInterface {
 		l2.setForeground(Color.red); 
 		l2.setFont(new Font("Courier",Font.BOLD, 40));	
 		this.setPreferredSize(new Dimension(300, 300));
-	    this.setBackground(Color.orange);	
-	    this.add(l1);
-	    this.add(l2);
-	    l2.setVisible(false);
+	    	this.setBackground(Color.orange);	
+	    	this.add(l1);
+	    	this.add(l2);
+	    	l2.setVisible(false);
 	 
-    }  
+    	}  
 	
+/* Displays Label saying "Hi" when flag is true */
+	@Override
 	public void sayHi(boolean flag)
-    { 
-			l2.setVisible(flag);
-    } 
+    	{ 
+		l2.setVisible(flag);
+    	} 
 }
